0102747687c36c548a687c1c135e80b5ddc60199
Orlando Iovino <orlando.iovino@yahoo.it>
2013-02-07 15:08:39 +0100

    (1) guidaemacsauctex.bib: risulta modificato perché Notepad++ non lo vedeva come UTF8, così ho fatto una conversione;
    (2) guidaemacsauctex.tex: ho modificato qualcosa riguardo il file .emacs, ma è ancora tutto *molto* provvisorio
    
    (3) style/guidatematica.el: ho iniziato a scrivere questo file di stile per la classe guidatematica. Deve essere completato e verificato. In futuro magari lo aggiungiamo alla classe ufficiale

---
+++ style/guidatematica.el
@@ -0,0 +1,143 @@
+;;; guidatematica.el --- AUCTeX style for `guidatematica.cls'
+
+;; Copyright (C) 2013 Free Software Foundation, Inc.
+
+;; Author: Orlando Iovino <orlando[dot]iovino[at]yahoo[dot]it>
+;; Created: 2013-02-07
+;; Keywords: tex
+
+;; This file is no part of AUCTeX.
+
+;; AUCTeX is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; AUCTeX is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with AUCTeX; see the file COPYING. If not, write to the Free
+;; Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+;; 02110-1301, USA.
+
+;;; Commentary:
+
+;; This file adds support for the `guidatematica.cls'.
+
+;;; Code:
+
+(TeX-add-style-hook "guidatematica"
+ (lambda ()
+   (TeX-add-symbols
+    ;; Comandi
+    '("ProvidesFile" "Nome del file" [ "aaaa/mm/gg v.x.y Descrizione" ])
+    '("subtitle" "Sottotitolo")
+    '("GetFileInfo" "Nome del file")
+    '("meta" "Argomento")
+    '("marg" "Argomento")
+    '("oarg" "Argomento")
+    '("garg" "x,y")
+    '("comando" "Comando (con \)")
+    '("cs" "Comando (senza \)")
+    '("Arg" "Argomento")
+    '("setcopymark" "Testo del copymark")
+    '("showindexmarks")
+    '("hideindexmarks")
+    ; - dubbio - '("texttm")
+    ; - dubbio - '("textttv")
+    '("tvfamily")
+    '("textl" "Numero")
+    '("prebibhook" "Testo")
+    '("ambstyle" "Nome dell'ambiente")
+    '("classstyle" "Nome della classe")
+    '("filestyle" "Nome del file")
+    '("packstyle" "Nome del pacchetto")
+    '("progstyle" "Nome del programma")
+    '("opzstyle" "Nome dell'opzione")
+    '("contastyle" "Nome del contatore")
+    '("stilestyle" "Nome dello stile di pagina")
+    '("numeristyle" "Nome dello stile di numerazione")
+    '("umisurastyle" "Unità di misura")
+    '("chiavestyle" "Nome della chiave")
+    '("descrittorestyle" "Codice del descrittore")
+    '("posizionestyle" "Codice di posizionamento")
+    '("allineamentostyle" "Codice di allineamento")
+    ;; Comandi collegati all'indice analitico
+    '("opz" "Nome dell'opzione")
+    '("conta" "Nome del contatore")
+    '("stile" "Nome dello stile di pagina")
+    '("numeri" "Nome dello stile di numerazione")
+    '("misura" "Unità di misura") ; --> chiedere se è umisura
+    '("chiave" "Nome della chiave")
+    '("descrittore" "Codice del descrittore")
+    '("posizione" "Codice di posizionamento")
+    '("allineamento" "Codice di allineamento")
+    '("amb" "Nome dell'ambiente")
+    '("class" "Nome della classe")
+    '("file" "Nome del file")
+    '("pack" "Nome del pacchetto")
+    '("prog" "Nome del programma")
+    ;; [TODO] RIPRENDI DA description
+    ;; [TODO] Ambienti
+    )
+   ;; Fontification
+   (when (and (featurep 'font-latex)
+	      (eq TeX-install-font-lock 'font-latex-setup))
+     (font-latex-add-keywords '(;; Comandi
+				("ProvidesFile" "{[")
+				("subtitle" "{")
+				("GetFileInfo" "{")
+				("meta" "{")
+				("marg" "{")
+				("oarg" "{")
+				("garg" "{")
+				("comando" "{")
+				("cs" "{")
+				("Arg" "{")
+				("setcopymark" "{")
+				("showindexmarks")
+				("hideindexmarks")
+				; - dubbio - ("texttm")
+				; - dubbio - ("textttv")
+				("tvfamily")
+				("textl" "{")
+				("prebibhook" "{")
+				("ambstyle" "{")
+				("classstyle" "{")
+				("filestyle" "{")
+				("packstyle" "{")
+				("progstyle" "{")
+				("opzstyle" "{")
+				("contastyle" "{")
+				("stilestyle" "{")
+				("numeristyle" "{")
+				("umisurastyle" "{")
+				("chiavestyle" "{")
+				("descrittorestyle" "{")
+				("posizionestyle" "{")
+				("allineamentostyle" "{")
+				;; Comandi collegati all'indice analitico
+				("opz" "{")
+				("conta" "{")
+				("stile" "{")
+				("numeri" "{")
+				("misura" "{") ; --> chiedere se è umisura
+				("chiave" "{")
+				("descrittore" "{")
+				("posizione" "{")
+				("allineamento" "{")
+				("amb" "{")
+				("class" "{")
+				("file" "{")
+				("pack" "{")
+				("prog" "{")
+				)
+			      'function))))
+
+(defvar LaTeX-guidatematica-class-options '("ipertesto")
+  "Class options for the guidatematica class.")
+
+;;; guidatematica.el ends here
